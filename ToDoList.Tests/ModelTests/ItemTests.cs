a45e4bbf236c68723f12658c94c41c1db5a7850b
Kevin Davis <thekidnamedkd@gmail.com>
2020-07-22 10:46:56 -0700

    fail test for for finding items.
    Co-authored-by: bwhite2140@outlook.com <bwhite2140@outlook.com>
    Co-authored-by: BAPotts <pottsbeverly@gmail.com>

--- ToDoList.Tests/ModelTests/ItemTests.cs
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -1,11 +1,17 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using ToDoList.Models;
+
 
 namespace ToDoList.Tests
 {
   [TestClass]
-  public class ItemTests
+  public class ItemTests : IDisposable
   {
+    public void Dispose()
+    {
+      Item.ClearAll();
+    }
 
     [TestMethod]
     public void ItemConstructor_CreatesInstanceOfItem_Item()
@@ -26,6 +32,27 @@ namespace ToDoList.Tests
 
       //Assert
       Assert.AreEqual(description, result);
+    }
+    [TestMethod]
+    public void GetId_ItemsInstantiateWithAnIdAndGetterReturns_Int()
+    {
+      string description = "Walk the dog.";
+      Item newItem = new Item(description);
+      int result = newItem.Id;
+      Assert.AreEqual(1, result);
+    }
+
+    [TestMethod]
+    public void Find_ReturnsCorrectItem_Item()
+    {
+      string description01 = "Walk the dog";
+      string description02 = "Wash the dishes";
+      Item newItem1 = new Item(description01);
+      Item newItem2 = new Item(description02);
+
+      Item result = new Item("Incorrect test item");
+
+      Assert.AreEqual(newItem2, result);
     }
 // commit changes
   }
